7ea69d358d2d2f94a43d7e4672fd3d97b40fce49
alitnp <alitnp@umail.com>
2020-12-08 18:08:16 +0330

    dumy prices if error

--- src/modules/profitCalc.js
+++ src/modules/profitCalc.js
@@ -11,18 +11,23 @@ export default async function profitCalc(
 		};
 	}
 
-	const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-	const targetUrl =
-		"https://www.coincalculators.io/api?name=bitcoin&hashrate=1000000000000";
+	try {
+		const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+		const targetUrl =
+			"https://www.coincalculators.io/api?name=bitcoin&hashrate=1000000000000";
 
-	const res = await axios.get(proxyUrl + targetUrl).catch((error) => {
+		const res = await axios.get(proxyUrl + targetUrl).catch((error) => {
+			console.log(error);
+		});
+
+		const price = res.data.exchanges[0].buyPrice;
+		const dailyProfit = res.data.rewardsInDay.toFixed(8);
+
+		sessionStorage.price = price;
+		sessionStorage.dailyProfit = dailyProfit;
+		return { price, dailyProfit };
+	} catch (error) {
 		console.log(error);
-	});
-
-	const price = res.data.exchanges[0].buyPrice;
-	const dailyProfit = res.data.rewardsInDay.toFixed(8);
-
-	sessionStorage.price = price;
-	sessionStorage.dailyProfit = dailyProfit;
-	return { price, dailyProfit };
+		return { price: 18000, dailyProfit: 0.00000693 };
+	}
 }
--- src/modules/rialRate.js
+++ src/modules/rialRate.js
@@ -4,12 +4,17 @@ export default async function rialRate() {
 	if (sessionStorage.rialRate) {
 		return sessionStorage.rialRate;
 	}
-	const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-	const targetUrl = "https://www.megaweb.ir/api/money";
-	const { data } = await axios.get(proxyUrl + targetUrl).catch((error) => {
+	try {
+		const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+		const targetUrl = "https://www.megaweb.ir/api/money";
+		const { data } = await axios.get(proxyUrl + targetUrl).catch((error) => {
+			console.log(error);
+		});
+		const rialRate = parseInt(data.sell_usd.price.replace(",", ""));
+		sessionStorage.rialRate = rialRate;
+		return rialRate;
+	} catch (error) {
 		console.log(error);
-	});
-	const rialRate = parseInt(data.sell_usd.price.replace(",", ""));
-	sessionStorage.rialRate = rialRate;
-	return rialRate;
+		return 250000;
+	}
 }
